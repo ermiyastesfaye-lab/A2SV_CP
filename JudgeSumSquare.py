f443d349c59dc27aaee1d0c295d9894ea34cfc3d
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2024-05-02 23:38:09 +0300

    Create JudgeSumSquare.py

---
+++ JudgeSumSquare.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a = 0
+        b = int(math.sqrt(c))
+        while a <= b:
+            if a*a + b*b == c:
+                return True
+            elif a*a + b*b > c:
+                b-=1
+            else:
+                a+=1
+        return False
+        
