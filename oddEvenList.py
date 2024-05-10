959e2354cc6615b22d997d866b91e7cebb9f8d07
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2024-05-10 20:30:22 +0300

    Update oddEvenList.py

--- oddEvenList.py
+++ oddEvenList.py
@@ -1,8 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return None
-        
         odd = head
         even_head = even = head.next
         while even and even.next:
