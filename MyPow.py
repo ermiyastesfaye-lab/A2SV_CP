95b909ef5b88c4dd436a85034b033c1b9f66c00b
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2024-07-12 01:49:33 +0300

    Update MyPow.py

--- MyPow.py
+++ MyPow.py
@@ -1,3 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        if n < 0:
+            return 1 / self.power(x, -n)
+        else:
+            return self.power(x, n)
+    
+    def power(self, x, n):
+        if n == 0:
+            return 1
+        half_pow = self.power(x, n // 2)
+        if n % 2 == 0:
+            return half_pow * half_pow
+        else:
+            return half_pow * half_pow * x
