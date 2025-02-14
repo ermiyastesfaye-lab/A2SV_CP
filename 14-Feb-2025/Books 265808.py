dc76efb35ddb722014973f43e3b9debdb4381041
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-14 15:44:39 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 265808.py
@@ -0,0 +1,27 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+def books(arr, time):
+    left = 0
+    window = 0
+    count = 0
+    books = 0
+    for right in range(len(arr)):
+        books += arr[right]
+        while books > time:
+            books-=arr[left]
+            left+=1
+
+        count = right - left + 1
+        window = max(window, count)
+        
+    return window
+
+n, time = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+print(books(arr, time))
+
+    
+        
+    
+    
+    
