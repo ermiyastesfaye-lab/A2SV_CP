ee2b36b7f579072a28bec6065a0d3c60fc0d75fa
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-14 22:33:35 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 14-Feb-2025/Alternating Subsequence 266657.py
@@ -0,0 +1,22 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+def alternatingSubsequence(arr, n):
+    def sign(num):
+        return num >= 0
+    left = 0
+    right = left
+    ans = 0
+    while right < len(arr):
+        maxx = float('-inf')
+        while right < len(arr) and sign(arr[right]) == sign(arr[left]):
+            maxx = max(arr[right], maxx)
+            right+=1
+        ans+=maxx
+        left = right
+    return ans
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(alternatingSubsequence(arr, n))
