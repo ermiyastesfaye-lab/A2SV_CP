aa07c478318e275f2721a0a9a29ffe4bcb84d0a4
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-18 15:25:26 +0300

    Created 'A - Nth Digit in Sequence' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/A - Nth Digit in Sequence 271683.py
@@ -0,0 +1,11 @@
+# Problem: A - Nth Digit in Sequence - https://codeforces.com/gym/588468/problem/A
+
+def nDigit(n):
+    s = []
+    for i in range(1, n+1):
+        s.append(str(i))
+    s = ''.join(s)
+    return s[n-1]
+
+n = int(input())
+print(nDigit(n))
