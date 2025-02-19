49fd58bd5a38b02cca83d5b770927af2a4b9e1b9
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-19 16:41:32 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Feb-2025/B - Anansi and Trip-Photographs 274429.py
@@ -0,0 +1,21 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+def anasi(arr, d):
+    arr.sort()
+    right = len(arr)//2
+    left = 0
+    while right < len(arr):
+        if arr[right] - arr[left] < d:
+            return "NO"
+        left+=1
+        right+=1
+    return "YES"
+    
+    
+
+
+t = int(input())
+for _ in range(t):
+    r, d = list(map(int, input().split()))
+    arr =  list(map(int, input().split()))
+    print(anasi(arr, d))
