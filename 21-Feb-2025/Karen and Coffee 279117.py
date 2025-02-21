4461538b0109f1b8cc3b25d88dcb2fc15fbb42d3
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-21 20:10:02 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 21-Feb-2025/Karen and Coffee 279117.py
@@ -0,0 +1,36 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+
+arr = list(map(int, input().split()))
+recipes = []
+ques = []
+prefix =[0] * (200000 + 2)
+for _ in range(arr[0]):
+    lst1 = list(map(int, input().split()))
+    prefix[lst1[0]-1] += 1
+    prefix[lst1[1]] -= 1
+    recipes.append(lst1)
+
+for _ in range(arr[2]):
+    lst2 = list(map(int, input().split()))
+    ques.append(lst2)
+
+for i in range(1, len(prefix)-1):
+    prefix[i] += prefix[i - 1]
+for i in range(len(prefix)-1):
+    if prefix[i] >= arr[1]:
+        prefix[i] = 1
+    else:
+        prefix[i] = 0
+for i in range(1, len(prefix)-1):
+    prefix[i] += prefix[i-1]
+
+for i in ques:
+    ans = prefix[i[1]-1] - prefix[i[0]-2]
+    print(ans)
+
+
+
+
+
+
