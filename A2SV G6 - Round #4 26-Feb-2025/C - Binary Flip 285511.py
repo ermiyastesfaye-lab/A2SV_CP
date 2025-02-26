6450a3d3618ce73b97b7ce64d68fafe2b78a5cbe
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-26 23:03:16 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/C - Binary Flip 285511.py
@@ -0,0 +1,48 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+def binaryFlip(a, b):
+    prefix = [0] * (n + 1)
+    summ = 0
+    for i in range(len(a)):
+        summ += int(a[i])
+        prefix[i] = summ
+    flip = 0
+    for i in range(len(a)-1, -1, -1):
+        if flip %2 != 0:
+            if a[i] == '0':
+                curr = '1'
+            else:
+                curr = '0'
+            if curr != b[i]:
+                if (i + 1)/2 == prefix[i]:
+                    flip+=1
+                else:
+                    return "NO"
+        else:
+             if a[i] != b[i]:
+                if (i + 1)/2 == prefix[i]:
+                    flip+=1
+                else:
+                    return "NO"
+    return "YES"
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input()
+    b = input()
+    print(binaryFlip(a, b))
+   
+
+                
+            
+
+
+
+
+
+        
+            
+    
+
+
