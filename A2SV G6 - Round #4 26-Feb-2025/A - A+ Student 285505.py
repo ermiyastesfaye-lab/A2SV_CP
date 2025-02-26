23a8b341ad5cbc7da092ec25b0bed0fe8e548270
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-26 23:02:07 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/A - A+ Student 285505.py
@@ -0,0 +1,18 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+t = int(input())
+for _ in range(t):
+    arr = list(map(int, input().split()))
+    maxx = max(arr)
+    for i in range(len(arr)):
+        if arr.count(maxx) > 1:
+            if arr[i] == maxx:
+                print(1, end=" ")
+                continue
+        elif arr.count(maxx) == 1:
+            if arr[i] == maxx:
+                print(0, end=" ")
+                continue
+        print(maxx - arr[i] + 1, end=" ")
+    print()
+    
