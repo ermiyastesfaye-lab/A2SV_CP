f4fafb1649274f527f8ea4a706c3147040a0f2d0
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-02-26 23:02:52 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/D - Minimize the Distance 285508.py
@@ -0,0 +1,18 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n= int(input())
+arr =list(map(int, input().split()))
+arr.sort()
+prefix = [0] * (n + 1)
+if len(arr) % 2 != 0:
+    mid = arr[len(arr)//2]
+else:
+    dif = (arr[len(arr)//2] + (arr[(len(arr)//2)-1]))/2
+    mid = dif
+ans = 0
+if len(arr) == 1:
+    print(arr[0])
+for i in range(len(arr)):
+    if mid < arr[i]:
+        print(arr[i-1])
+        break
