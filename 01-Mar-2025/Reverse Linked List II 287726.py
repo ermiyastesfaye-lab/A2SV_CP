2c74c007eb6a1cca9a93ec4ff472baed4c6b1028
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-01 20:43:56 +0300

    Created 'Reverse Linked List II' by A2SV Hub

---
+++ 01-Mar-2025/Reverse Linked List II 287726.py
@@ -0,0 +1,34 @@
+# Problem: Reverse Linked List II - https://leetcode.com/problems/reverse-linked-list-ii/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if head is None:
+            return None
+        temp = head
+        prev = None
+        for _ in range(left - 1):
+            prev = temp
+            temp = temp.next
+        before_sublist = prev
+        last_node_of_sublist = temp
+        current = temp
+        next_node = None
+        for _ in range(right - left + 1):
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        
+        if before_sublist:
+            before_sublist.next = prev
+        else:
+            head = prev
+        
+        last_node_of_sublist.next = current
+        
+        return head
