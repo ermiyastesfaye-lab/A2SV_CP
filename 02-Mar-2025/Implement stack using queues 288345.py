8df6a40955c0f7313b750d556876047b725f6ea2
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-02 22:13:36 +0300

    Created 'Implement stack using queues' by A2SV Hub

---
+++ 02-Mar-2025/Implement stack using queues 288345.py
@@ -0,0 +1,47 @@
+# Problem: Implement stack using queues - https://leetcode.com/problems/implement-stack-using-queues/
+
+import queue;
+
+class MyStack:
+
+    def __init__(self):
+        self.q1= queue.Queue()
+        self.q2 = queue.Queue()
+        
+        
+    def push(self, x: int) -> None:
+        self.q1.put(x)
+        return None
+
+    def pop(self) -> int:
+        if self.q1.empty():
+            return None
+        while self.q1.qsize() > 1:
+            self.q2.put(self.q1.get())
+        lastPop = self.q1.get()
+        self.q1, self.q2 = self.q2, self.q1
+        return lastPop
+    
+    def top(self) -> int:
+        while not self.q1.empty():
+            lastElement = self.q1.get()
+            self.q2.put(lastElement)
+        
+        self.q1, self.q2 = self.q2, self.q1
+        return lastElement
+        
+
+    def empty(self) -> bool:
+        if self.q1.empty():
+            return True
+        return False
+
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
