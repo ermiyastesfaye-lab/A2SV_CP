90cecb780420e01ceb97cb0556c10300167c1f4f
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-08 00:26:40 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 07-Mar-2025/Odd Subarrays 297669.py
@@ -0,0 +1,18 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    cnt = 0
+    if arr == sorted(arr):
+        print(0)
+        continue
+    i = 1
+    while i < len(arr):
+        if arr[i] < arr[i - 1]:
+            cnt += 1
+            i+=2
+        else:
+            i+=1
+    print(cnt)
