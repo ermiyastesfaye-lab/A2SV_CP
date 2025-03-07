1429feb6599a433117b7b75e7a6b4885c538baaf
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-07 20:02:57 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 07-Mar-2025/Array Splitting 297217.py
@@ -0,0 +1,15 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+n, k = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+new = []
+if k == 1:
+    print(arr[-1] - arr[0])
+    exit()
+for i in range(1, len(arr)):
+    new.append(arr[i] - arr[i - 1])
+new.sort()
+largest = sum(new[-k+1:])
+print(arr[-1] - arr[0] - largest)
+
+    
