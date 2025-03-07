a1e61381eb5c67c7554ccafbae123edcb8ff9c56
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-07 17:43:58 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 07-Mar-2025/Kuriyama Mirai's Stones 296770.py
@@ -0,0 +1,24 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+arrS = sorted(arr)
+for i in range(1, len(arr)):
+    arr[i] += arr[i-1]
+arr.append(0)
+for i in range(1, len(arrS)):
+    arrS[i] += arrS[i - 1]
+arrS.append(0)
+m = int(input())
+question = []
+for i in range(m):
+    temp = list(map(int, input().split()))
+    question.append(temp)
+for i in question:
+    if i[0] == 1:
+        print(arr[i[2]-1] - arr[i[1]-2])
+    else:
+        print(arrS[i[2]-1] - arrS[i[1]-2])
+
+
+    
