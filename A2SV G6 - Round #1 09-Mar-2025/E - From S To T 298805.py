fc52327c88f12dc874f350e547b65f30b88f6154
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-09 20:57:02 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 09-Mar-2025/E - From S To T 298805.py
@@ -0,0 +1,36 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+def stot():
+    s = list(input())
+    t = list(input())
+    p = list(input())
+    if len(t) < len(s):
+        return "NO"
+    i = 0
+    j = 0
+    while j < len(t):
+        if i >= len(s):
+            if t[j] in p:
+                idx = p.index(t[j])
+                p[idx] = -1
+                j+=1
+            else:
+                return "NO"
+        else:
+            if s[i] != t[j]:
+                if t[j] in p:
+                    idx = p.index(t[j])
+                    p[idx] = None   
+                    j+=1
+                else:
+                    return "NO"
+            else:
+                i+=1
+                j+=1
+    if i < len(s):
+        return "NO"
+    return "YES"          
+        
+t = int(input()) 
+for k in range(t):
+    print(stot())  
