c21a58e3cda263f6381d47217ddc233f25dc854b
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-10 23:43:15 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/D - Life Across the Stars 300890.py
@@ -0,0 +1,30 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+
+
+n = int(input())
+tot = []
+dic = defaultdict(int)
+for _ in range(n):
+    arr = list(map(int, input().split()))
+    tot.append(arr)
+for i in tot:
+    dic[i[0]]+=1
+    dic[i[1]]-=1
+dic = dict(sorted(dic.items()))
+lst1 = list(dic.keys())
+lst2 = list(dic.values())
+for i in range(1, len(lst2)):
+    lst2[i] += lst2[i - 1]
+idx = lst2.index(max(lst2))
+print(lst1[idx], lst2[idx])
+
+
+
+
+
+
+
+
+
