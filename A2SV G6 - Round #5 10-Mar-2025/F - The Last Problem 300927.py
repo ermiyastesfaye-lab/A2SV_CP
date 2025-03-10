eb60cb00cdfdce2e53bbda6cc7eb9dcba0db9cd0
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-11 00:20:27 +0300

    Created 'F - The Last Problem' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/F - The Last Problem 300927.py
@@ -0,0 +1,31 @@
+# Problem: F - The Last Problem - https://codeforces.com/gym/591928/problem/F
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+summ = 0
+for i in range(n):
+    summ += a[i] * b[i]
+
+rev = 0
+for center in range(n):
+    left, right = center-1, center+1
+    temp = 0
+    while left >= 0  and right < n:
+        temp += a[left]*b[right] + a[right]*b[left] - a[left] * b[left] - a[right] * b[right]
+        rev = max(rev, temp)
+        left -= 1
+        right += 1
+
+for center in range(n-1):
+    left, right = center, center+1
+    temp = 0
+    while left >= 0  and right < n:
+        temp += a[left]*b[right] + a[right]*b[left] - a[left] * b[left] - a[right] * b[right]
+        rev = max(rev, temp)
+        left -= 1
+        right += 1
+print(summ + rev)
+
+    
