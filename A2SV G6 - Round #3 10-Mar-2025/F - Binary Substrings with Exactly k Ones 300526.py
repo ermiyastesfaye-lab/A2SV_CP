15070120a8d96c3f77a2c7f7ca7e9403ddb0bafd
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-10 19:31:08 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 10-Mar-2025/F - Binary Substrings with Exactly k Ones 300526.py
@@ -0,0 +1,31 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+from collections import defaultdict
+
+def BinarySubstring(k, s):
+    left = 0
+    left2 = 0
+    ans = 0
+    ans2 = 0
+    cnt1 = 0
+    cnt2 = 0
+    for right in range(len(s)):
+            if s[right] == "1":
+                cnt1 += 1
+                cnt2+=1
+            while left2 <= right and cnt2 > k-1:
+                if s[left2] == "1":
+                    cnt2-=1
+                left2 += 1
+            ans2 += right - left2 + 1 
+            while left <= right and cnt1 > k:
+                if s[left] == "1":
+                    cnt1-=1
+                left+=1
+            ans += right - left + 1
+    return ans - ans2
+      
+
+k = int(input())
+s = input()
+print(BinarySubstring(k, s))
