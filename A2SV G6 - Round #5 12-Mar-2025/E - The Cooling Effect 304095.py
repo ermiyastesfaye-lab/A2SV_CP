35013144e1e0961335236edd8d402b9e6dc8b43e
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-12 18:26:08 +0300

    Created 'E - The Cooling Effect' by A2SV Hub

---
+++ A2SV G6 - Round #5 12-Mar-2025/E - The Cooling Effect 304095.py
@@ -0,0 +1,29 @@
+# Problem: E - The Cooling Effect - https://codeforces.com/gym/591928/problem/E
+
+t = int(input())
+
+for _ in range(t):
+  white_space = input()
+  n, k = list(map(int, input().split()))
+  indices = list(map(int, input().split()))
+  temps = list(map(int, input().split()))
+  arr1 = [float('inf')] * n
+  arr2 = [float('inf')] * n
+  
+  for i, num in enumerate(indices):
+    arr1[num - 1] = temps[i]
+    arr2[num - 1] = temps[i]
+
+  for i in range(1, n):
+    if arr1[i] > arr1[i - 1] + 1:
+      arr1[i] = arr1[i - 1] + 1
+      
+  for i in range(n - 2, -1, -1):
+    if arr2[i] > arr2[i + 1] + 1:
+      arr2[i] = arr2[i + 1] + 1
+      
+  ans = []
+  for x, y in zip(arr1, arr2):
+    ans.append(min(x, y))
+    
+  print(*ans)
