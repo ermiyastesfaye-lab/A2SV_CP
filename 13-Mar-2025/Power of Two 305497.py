8e02d4b3a68b70a2f09aac532119eed13976004b
ermiyastesfaye-lab <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-03-13 19:58:41 +0300

    Created 'Power of Two' by A2SV Hub

---
+++ 13-Mar-2025/Power of Two 305497.py
@@ -0,0 +1,13 @@
+# Problem: Power of Two - https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        return self.rec(n)
+    def rec(self, n):
+        if n == 1:
+            return True
+        if n%2 != 0:
+            return False
+        return self.rec(n//2)
