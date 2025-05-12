22029269652764a730f06d0567c9c4fc53f9a927
Ermiyas Tesfaye <120595070+ermiyastesfaye-lab@users.noreply.github.com>
2025-05-12 19:15:15 +0300

    Created 'Spanning Tree' by A2SV Hub

---
+++ 12-May-2025/Spanning Tree 353824.py
@@ -0,0 +1,80 @@
+# Problem: Spanning Tree - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/E
+
+import sys
+import math
+from collections import Counter, defaultdict, deque
+
+inputTaker = sys.stdin.readline
+
+def inp(): return int(inputTaker())
+def linp(): return list(map(int, inputTaker().split()))
+def sinp(): return inputTaker().strip()
+def minp(): return map(int, inputTaker().split())
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [0]*size
+    
+    def find(self, x):
+        if self.root[x] == x:
+            return x
+        val = self.find(self.root[x])
+        self.root[x] = val
+        return val
+    
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx == rooty:
+            return False
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.root[rooty] = rootx
+            elif self.rank[rooty] > self.rank[rootx]:
+                self.root[rootx] = rooty
+            else:
+                self.root[rootx] = rooty
+                self.rank[rooty] += 1
+        return True
+def solve():
+    n, m = linp()
+    uf = UnionFind(n)
+    lst = []
+    for i in range(m):
+        b, c, w = linp()
+        lst.append((b, c, w))
+    lst.sort(key=lambda x: x[2])
+    root = [i for i in range(n + 1)]
+    def find(x):
+        if root[x] == x:
+            return x
+        val = find(root[x])
+        root[x] = val
+        return val
+    def union(x, y):
+        rootx = find(x)
+        rooty = find(y)
+        if rootx == rooty:
+            return False
+        root[rooty] = rootx
+        return True
+    ans = 0
+    edg = 0
+    for b, c, w in lst:
+        if edg == n-1:
+            break
+        if union(b, c):
+            ans += w
+            edg += 1
+    print(ans)
+
+
+        
+def main():
+    t = 1 
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
